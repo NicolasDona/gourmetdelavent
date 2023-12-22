27753f606e3c668d9579549dd55dc222fdeff1b9
braizou <amirat.mehdi@gmail.com>
2023-12-22 15:51:27 +0100

    ok

--- public/assets/js/animation.js
+++ public/assets/js/animation.js
@@ -16,14 +16,21 @@ console.log(dateDayNumber);
 
 // ! Evènements
 
-openCase.addEventListener('click', () => {
-    if (openCase.getAttribute('data-date') <= dateDayNumber) {
-    openDoor();
-    localStorage.setItem('dayOpened', openCase.getAttribute('data-date')); // mémoriser la case ouverte au click
-    }
-})
+openCase.addEventListener('click', (event) => {
+    const clickedCase = event.currentTarget;
 
-imgToExtend.addEventListener('click', zoomIn);
+    if (clickedCase.getAttribute('data-date') <= dateDayNumber) {
+        openDoor(clickedCase);
+        localStorage.setItem('dayOpened', clickedCase.getAttribute('data-date'));
+    }
+});
+
+
+imgToExtend.addEventListener('click', (event) => {
+    const clickedCase = event.currentTarget.parentElement;
+    zoomIn(clickedCase);
+});
+
 
 fullsizeImg.addEventListener('click', zoomOut);
 
@@ -31,15 +38,11 @@ fullsizeImg.addEventListener('click', zoomOut);
 
 // ! Fonctions
 
-function openDoor() {
+function openDoor(clickedCase) {
+    const door = clickedCase.querySelector('img');
     door.classList.add('open');
 }
 
-function zoomIn() {
-    fullsizeImg.classList.remove('d-none');
-    fullsizeImg.classList.remove('zoomOut');
-    fullsizeImg.classList.add('zoomIn');
-}
 
 function zoomOut() {
     fullsizeImg.classList.remove('zoomIn');
@@ -48,3 +51,53 @@ function zoomOut() {
         fullsizeImg.classList.add('d-none');
     }, 1000);
 }
+
+function zoomIn(clickedCase) {
+    const dayNumber = clickedCase.getAttribute('data-date');
+
+    fetch('./public/assets/json/base.json')
+        .then(response => response.json())
+        .then(data => {
+            const selectedItem = data.find(item => item.id == dayNumber);
+
+            updateCard(selectedItem);
+        })
+        .catch(error => console.error('Erreur lors du chargement du fichier JSON:', error));
+}
+
+
+function updateCard(data) {
+
+    const fullsizeDiv = document.querySelector('.fullsize');
+    const cardImg = fullsizeDiv.querySelector('.card-img-top');
+    const cardTitle = fullsizeDiv.querySelector('.card-title');
+    const cardText = fullsizeDiv.querySelector('.card-text');
+    const btnPrimary = fullsizeDiv.querySelector('.btn-primary');
+
+    cardImg.src = `./public/assets/img/img_big/${data.img}.jpg`;
+    cardTitle.textContent = data.title;
+    cardText.textContent = data.description;
+
+    btnPrimary.addEventListener('click', () => {
+        window.open(data.recipe, '_blank');
+    });
+    
+
+    fullsizeDiv.classList.remove('d-none');
+    fullsizeDiv.classList.remove('zoomOut');
+    fullsizeDiv.classList.add('zoomIn');
+}
+// ! Evènements
+
+const allOpenCases = document.querySelectorAll('.container__door');
+
+allOpenCases.forEach(openCase => {
+    openCase.addEventListener('click', (event) => {
+        const clickedCase = event.currentTarget;
+
+        if (clickedCase.getAttribute('data-date') <= dateDayNumber) {
+            openDoor(clickedCase);
+            localStorage.setItem('dayOpened', clickedCase.getAttribute('data-date'));
+        }
+    });
+});
--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,36 @@
 // service-worker.js
 
 const CACHE_NAME = 'my-cache';
+const baseDir = './public/assets/img/img_big/';
+const baseDir1 = './public/assets/img/thumb/';
 const urlsToCache = [
   '/',
   './index.html',
+  './public/assets/img/case-01.jpg',
+  './public/assets/img/case-02.jpg',
+  './public/assets/img/case-03.jpg',
+  './public/assets/img/case-04.jpg',
+  './public/assets/img/case-05.jpg',
+  './public/assets/img/case-06.jpg',
+  './public/assets/img/case-07.jpg',
+  './public/assets/img/case-08.jpg',
+  './public/assets/img/case-09.jpg',
+  './public/assets/img/case-10.jpg',
+  './public/assets/img/case-11.jpg',
+  './public/assets/img/case-12.jpg',
+  './public/assets/img/case-13.jpg',
+  './public/assets/img/case-14.jpg',
+  './public/assets/img/case-15.jpg',
+  './public/assets/img/case-16.jpg',
+  './public/assets/img/case-17.jpg',
+  './public/assets/img/case-18.jpg',
+  './public/assets/img/case-19.jpg',
+  './public/assets/img/case-20.jpg',
+  './public/assets/img/case-21.jpg',
+  './public/assets/img/case-22.jpg',
+  './public/assets/img/case-23.jpg',
+  './public/assets/img/case-24.jpg',
+
 //ajouter ici tout les fichiers qui seront téléchargé pour une utilisation offline (ex:css,js,bootstrap etc...)
 ];
 
